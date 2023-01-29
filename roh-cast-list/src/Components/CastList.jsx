24a9b9bd652a8cc436ede713b2deb0f6895bc719
Amelss <ameleyka@gmail.com>
2023-01-29 14:05:13 +0000

    Getting creatives roles and names

--- roh-cast-list/src/Components/CastList.jsx
+++ roh-cast-list/src/Components/CastList.jsx
@@ -1,47 +1,77 @@
 import React from 'react'
+import { useState } from 'react';
 import "../index.css";
+
+
+
+   
 
 export default function CastList() {
 
-
-    
-    const getCastList = () => {
-     
+    const [castList, setCastList] = useState({})
+    const [creatives, setCreatives] = useState([])
+   
+   
+    async function getCastData ()  {
         fetch(
-          `https://www.roh.org.uk/api/event-details?slug=turandot-by-andrei-serban`
+            `https://www.roh.org.uk/api/event-details?slug=turandot-by-andrei-serban`
         )
-          .then((response) => response.json())
-          .then((data) => {
-            console.log(data.data);
-          });
+            .then((response) => response.json())
+            .then((data) => {
+                setCastList(data)
+                setCreatives(data.included)
+             
+                console.log(data);
+                
+            });
+      
     }
 
 
+
+
+
+    
+
+
+    const filteredCreatives = creatives.filter(
+        (item) => item.type === "creatives");
+   
+    
   return (
+    <div>
       <div>
-          <div>
-        <button onClick={getCastList}>Test</button>
-              <div>
-                  {/* Basic Layout of page with placeholders */}
+        <button onClick={getCastData}>Test</button>
+        <div>
+          
                   
-                <h1>Title: </h1>
-                <p>Date:</p>
-                <p>Description:</p>
+     
+   
 
-                <h1>Creative Title:</h1>
-                <p>Creatives Names and Roles:</p>
+          {/* Basic Layout of page with placeholders */}
+    
+               
+                
+                  <h1>Title:{}</h1>
+          <p>Date:</p>
+          <p>Description:</p>
 
-                <h1>Cast Title</h1>
-                <p>Cast Names and Roles:</p> 
-                  </div>
-            
-      
-           
-          </div>
-       
+          
+                  <h1>Creatives:</h1>
+                  {filteredCreatives.map((creator) => (
+                      <div key={creator.id}>
+                          <p>
+                              {creator.attributes.name} - {creator.attributes.role}
+                          </p>
+                      </div>
 
-
-
+                  ))}
+                  <h1>Cast Title</h1>
+                  
+                
+          <p>Cast Names and Roles:</p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
