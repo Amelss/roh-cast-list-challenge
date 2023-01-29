1385e2ceba085bf9fb440565060c81aa709cbaca
Amelss <ameleyka@gmail.com>
2023-01-29 16:16:54 +0000

    Rendering title and Short Description

--- roh-cast-list/src/Components/CastList.jsx
+++ roh-cast-list/src/Components/CastList.jsx
@@ -1,54 +1,56 @@
-import React from 'react'
-import { useEffect } from 'react';
-import { useState } from 'react';
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 import "../index.css";
 
-
-
-   
-
 export default function CastList() {
+  const [castList, setCastList] = useState({});
+  const [creatives, setCreatives] = useState([]);
+  const [castMembers, setCastMembers] = useState([]);
+  const [attributes, setAttributes] = useState({});
 
-    const [castList, setCastList] = useState({});
-    const [creatives, setCreatives] = useState([]);
-    const [castMembers, setCastMembers] = useState([]);
-   
-   
-    const  getCastData = ()  => {
-        fetch(
-            `https://www.roh.org.uk/api/event-details?slug=turandot-by-andrei-serban`
-        )
-            .then((response) => response.json())
-            .then((data) => {
-                setCastList(data);
-                setCreatives(data.included);
-                setCastMembers(data.included);
-                
-            });
-      
-    }
-    useEffect(() => {
-     getCastData()
- }, [])
 
-    const filteredCreatives = creatives.filter(
-        (item) => item.type === "creatives");
-   
-    const filteredCastMembers = castMembers.filter(
-      (item) => item.type === "castRoles"
-    );
+  async function getCastData() {
+    await fetch(
+      `https://www.roh.org.uk/api/event-details?slug=turandot-by-andrei-serban`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setCastList(data.data);
+        setCreatives(data.included);
+        setCastMembers(data.included);
+        setAttributes(data.data.attributes);
+        console.log(data.data.attributes);
+      })
+      .catch((err) => console.log(err));
+  }
+  useEffect(() => {
+    getCastData();
+  }, []);
+
+  // const { data } = castList;
+  // console.log(data)
+
+  const filteredCreatives = creatives.filter(
+    (item) => item.type === "creatives"
+  );
+
+  const filteredCastMembers = castMembers.filter(
+    (item) => item.type === "castRoles"
+  );
+
+  // console.log({castList, id:castList.data.id})
   return (
     <div>
       <div>
-        <button onClick={getCastData}>Test</button>
         <div>
           {/* Basic Layout of page with placeholders */}
 
-          <h1>Title:{}</h1>
-          <p>Date:</p>
-          <p>Description:</p>
+          <h1>Title:{attributes.title}</h1>
+          <p>Date: 10/03/2023</p>
+                  Description:{ attributes.shortDescription}
 
-          <h1>Creatives:</h1>
+          <h1>Creatives</h1>
           {filteredCreatives.map((creator) => (
             <div key={creator.id}>
               <p>
